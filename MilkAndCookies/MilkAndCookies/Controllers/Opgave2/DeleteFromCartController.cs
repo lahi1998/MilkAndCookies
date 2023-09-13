47019e81c0e11ae8d09eb74b32356bdba04654de
lahi1998 <95356006+lahi1998@users.noreply.github.com>
2023-09-13 10:31:36 +0200

    Add files via upload

---
+++ MilkAndCookies/MilkAndCookies/Controllers/Opgave2/DeleteFromCartController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MilkAndCookies.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MilkAndCookies.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MilkAndCookies.Controllers.Opgave2
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeleteFromCartController : ControllerBase
+    {
+
+        [HttpDelete("{productName}")]
+        public ActionResult<IEnumerable<Product>> RemoveFromCart(string productName)
+        {
+            // Retrieve the shopping cart from the session
+            var shoppingCart = HttpContext.Session.GetObjectFromJson<List<Product>>("ShoppingCart") ?? new List<Product>();
+
+            // Find the product in the cart by name and remove it
+            Product productToRemove = null;
+            foreach (var product in shoppingCart)
+            {
+                if (product.Name == productName)
+                {
+                    productToRemove = product;
+                    break; // Exit the loop when a matching product is found
+                }
+            }
+
+            if (productToRemove != null)
+            {
+                shoppingCart.Remove(productToRemove);
+                // Store the updated shopping cart in the session
+                HttpContext.Session.SetObjectAsJson("ShoppingCart", shoppingCart);
+            }
+
+
+            return Ok(shoppingCart);
+        }
+
+    }
+
+}
+
